9501b3b40a28d42e0e20c4458b575e3c5e7e7714
itamochi-raspi <takayuki.itamochi@gmail.com>
2018-02-04 14:58:15 +0900

    try to determin auto kotatsu on-off (not works well)

--- video_cap.py
+++ video_cap.py
@@ -5,9 +5,18 @@ from dropbox.files import WriteMode
 import sys
 from datetime import datetime
 
+def determin_onoff(single_channel_img, threshold, rate):
+    thed = cv2.threshold(single_channel_img, threshold, 255, cv2.THRESH_BINARY)[1]
+
+    nonzero_areas = thed.nonzero()
+    nonzero_area_count = len(nonzero_areas[0])
+
+    print(nonzero_area_count, thed.shape[0]*thed.shape[1])
+    return nonzero_area_count * 1.0 / (thed.shape[0]*thed.shape[1]) >= rate
+
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        print("python %s [ACCESS_TOKEN_FILE] [SAVE_FNAME]" % (sys.argv[1],))
+        print("python %s [ACCESS_TOKEN_FILE] [SAVE_FNAME] [KOTATSU_ON_ACCESS] [KOTATSU_OFF_ACCESS]" % (sys.argv[1],))
         sys.exit(0)
 
     access_token = None
@@ -15,9 +24,11 @@ if __name__ == "__main__":
     with open(sys.argv[1]) as fr:
         access_token = fr.readline().strip()
 
+    # open Dropbox instance
     dbx = dropbox.Dropbox(access_token)
     dbx.users_get_current_account()
-        
+
+    # try open VideoCapture 
     cap = cv2.VideoCapture(-1)
     while True:
         print("try to capture...")
@@ -29,16 +40,28 @@ if __name__ == "__main__":
         time.sleep(10)
         cap = cv2.VideoCapture(-1)
 
+    prev_on_state = None
+
     print("start to capture loop")
     while True:
         ret, frame = cap.read()
         if ret and frame is not None:
             updated_time = datetime.now()
-            print(str(frame.shape) + ":" + str(updated_time))
+
+            # determine kotatsu is on/off (not good work)
+            rchannel = frame[:,:,2]
+            is_on = determin_onoff(rchannel, 80, 0.02)
+            print(str(frame.shape) + ":" + str(updated_time) + ":is_on = " + str(is_on))
+            # write updated time to the image
             cv2.putText(frame, str(updated_time), (10,20), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,255))
+            # save image
             cv2.imwrite(save_fname, frame)
+
+            # upload to Dropbox
             with open(save_fname, "rb") as fr:
                 dbx.files_upload(fr.read(), '/' + save_fname, mode=WriteMode('overwrite', None))
+
+            prev_on_state = is_on
         else:
             print("failed to capture: " + str(datetime.now()))
             
